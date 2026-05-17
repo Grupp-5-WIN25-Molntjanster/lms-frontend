4c13b8a22417c6adba08f287025db779364f47f9
Riad Alrashed <m.riad.alrashed@gmail.com>
2026-05-17 05:33:47 +0200

    chore(ui): add component exports

---
+++ src/components/ui/index.ts
@@ -0,0 +1,6 @@
+export { Button } from "./Button";
+export { Badge } from "./Badge";
+export { Card } from "./Card";
+export { Input } from "./Input";
+export { Pagination } from "./Pagination";
+export { SidebarMenu, type SidebarItem } from "./SidebarMenu";
